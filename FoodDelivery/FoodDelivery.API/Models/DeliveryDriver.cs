1549df54670dd63e502b3cb5e3623dd5f08f651c
Jamiwal-3704 <ittansahil@gmail.com>
2026-05-06 14:52:35 +0530

    Scraffold Completed

---
+++ FoodDelivery/FoodDelivery.API/Models/DeliveryDriver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodDelivery.API.Models;
+
+public partial class DeliveryDriver
+{
+    public int DriverId { get; set; }
+
+    public string? DriverName { get; set; }
+
+    public string? DriverPhone { get; set; }
+
+    public string? DriverVehicle { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
